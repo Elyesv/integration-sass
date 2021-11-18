ea46e73b4f97e2d48a2192500a1ff867720776e2
Elyes Voisin <elyes.voisin@edu.devinci.fr>
2021-11-18 18:24:49 +0100

    :lipstick::dizzy: ADD fourth row + responsive + UPDATE animation for mobile

--- script/script.js
+++ script/script.js
@@ -14,3 +14,14 @@ cards.forEach(card =>{
         button.classList.add('display-none')
     })
 })
+
+cards.forEach(card =>{
+    card.addEventListener('touchstart', function (){
+        card.classList.toggle('selected-card')
+        var button = Array.from(card.children)
+        var button = button[5]
+        button.classList.toggle('display-none')
+    })
+})
+
+
